430ee27bd8a3f657a743d289315442b26aea4c2a
Ivan Juarez <ivan@madebyraygun.com>
2024-12-04 23:28:03 -0600

    feat: dot env support

--- src/config.py
+++ src/config.py
@@ -1,16 +1,32 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+########################
 # DSP Audio Configuration
-DSP_RATE = 44100                # Sampling rate in Hz
-DSP_FPS = 42                    # Frames per second
-DSP_N_ROLLING_HISTORY = 4       # Number of frames to keep in rolling buffer
-DSP_N_FFT_BINS = 1              # Number of Mel filterbank channels (5 LEDs)
-DSP_MIN_FREQUENCY = 20         # Minimum frequency for Mel filterbank in Hz
-DSP_MAX_FREQUENCY = 160       # Maximum frequency for Mel filterbank in Hz
-# MIN_FREQUENCY = 3000
-# MAX_FREQUENCY = 20000
-# MIN_FREQUENCY = 10000
-# MAX_FREQUENCY = 20000
-DSP_DEVICE_INDEX = 3            # Audio input device index
+#########################
+
+# Sampling rate in Hz
+DSP_RATE = 44100
+# Frames per second
+DSP_FPS = 42
+# Number of frames to keep in rolling buffer
+DSP_N_ROLLING_HISTORY = 4
+# Number of Mel filterbank channels (5 LEDs)
+DSP_N_FFT_BINS = 1
+# Minimum frequency for Mel filterbank in Hz
+DSP_MIN_FREQUENCY = 20
+# Maximum frequency for Mel filterbank in Hz
+DSP_MAX_FREQUENCY = 160
+# Audio device index to use
+DSP_DEVICE_INDEX = os.getenv("DSP_DEVICE_INDEX", 0)
+# Minimum volume threshold for audio input
 DSP_MIN_VOLUME_THRESHOLD = 1e-7
 
 
+########################
+# Global Configuration
+########################
+
+# The GPIO pin number for the LED status indicator (BCM numbering)
 LED_STATUS_GPIO_PIN = 5
