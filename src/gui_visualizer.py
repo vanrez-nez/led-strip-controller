9ea1b09016719dfe5d5b2a7dcf6f9ef0028090f8
Ivan Juarez <ivan@madebyraygun.com>
2024-12-05 00:56:27 -0600

    refact: testing blinking mode

--- src/gui_visualizer.py
+++ src/gui_visualizer.py
@@ -63,21 +63,21 @@ class GuiStripVisualizer:
         self.canvas.update()
 
 def main():
-    strip = Strip(50)
+    strip = Strip(num_leds=30, gpio_pin=18, dma_channel=10, pwm_channel=0)
     visualizer = GuiStripVisualizer(strip)
-    segment1 = Segment(strip, 0, 50)
+    segment1 = Segment(strip, 0, 30)
     # segment2 = Segment(strip, 40, 50, direction=-1)
     gPreset = GRADIENT_PRESETS["red_flash"]
     # gPreset = GRADIENT_PRESETS["es_vintage_57"]
     g = Gradient(colors=gPreset, resolution=255)
     blink_fx = BlinkFx(segment1, color=(255, 0, 0), interval=500, smooth=True, gradient=g)
-    blink_fx.set_mode("smooth_level")
-    meter_fx = MeterFx(segment1, gradient=g)
-    meter_fx.set_mode("meter_sides")
+    blink_fx.set_mode("strobe")
+    # meter_fx = MeterFx(segment1, gradient=g)
+    # meter_fx.set_mode("meter_sides")
     processor = DSPProcessor()
     effectManager = EffectManager(mode="auto_cycle", time_cycle_duration=30000)
     effectManager.add(blink_fx)
-    effectManager.add(meter_fx)
+    # effectManager.add(meter_fx)
     processor.start()
     loop = Loop()
 
