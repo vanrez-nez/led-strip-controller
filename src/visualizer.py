b0632494012e7ce6530b4333c40816975a21b3ce
Ivan Juarez <ivan@madebyraygun.com>
2024-12-06 20:20:18 -0600

    feat: implements multi segment to update strips with single effects

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,5 +1,6 @@
 from strip import Strip
 from segment import Segment
+from multi_segment import MultiSegment
 from effects.blink_fx import BlinkFx
 from effects.meter_fx import MeterFx
 from loop import Loop
@@ -13,22 +14,52 @@ def main():
     leftStrip = Strip(num_leds=120, gpio_pin=18, dma_channel=10, pwm_channel=0)
     rightStrip = Strip(num_leds=120, gpio_pin=21, dma_channel=5, pwm_channel=0)
 
-    segment_l1 = Segment(leftStrip, 0, 120)
-    segment_r1 = Segment(rightStrip, 0, 120)
+    segment_l1 = Segment(leftStrip, 0, 30)
+    segment_l2 = Segment(leftStrip, 60, 90)
+    leftMultisegment_1 = MultiSegment(mode="add")
+    leftMultisegment_1.addSegment(segment_l1)
+    leftMultisegment_1.addSegment(segment_l2)
+
+    segment_l3 = Segment(leftStrip, 30, 60)
+    segment_l4 = Segment(leftStrip, 90, 120)
+    leftMultisegment_2 = MultiSegment(mode="add")
+    leftMultisegment_2.addSegment(segment_l3)
+    leftMultisegment_2.addSegment(segment_l4)
+
+    segment_r1 = Segment(leftStrip, 0, 30)
+    segment_r2 = Segment(leftStrip, 60, 90)
+    rightMultisegment_1 = MultiSegment(mode="add")
+    rightMultisegment_1.addSegment(segment_r1)
+    rightMultisegment_1.addSegment(segment_r2)
+
+    segment_r3 = Segment(rightStrip, 30, 60)
+    segment_r4 = Segment(rightStrip, 90, 120)
+    rightMultisegment_2 = MultiSegment(mode="add")
+    rightMultisegment_2.addSegment(segment_r3)
+    rightMultisegment_2.addSegment(segment_r4)
+
     gPreset = GRADIENT_PRESETS["red_flash"]
     g = Gradient(colors=gPreset, resolution=255)
 
-    meter_fx_l1 = MeterFx(segment_l1, gradient=g)
+    meter_fx_l1 = MeterFx(leftMultisegment_1, gradient=g)
     meter_fx_l1.set_mode("meter_sides")
 
-    meter_fx_r1 = MeterFx(segment_r1, gradient=g)
+    meter_fx_l2 = MeterFx(leftMultisegment_2, gradient=g)
+    meter_fx_l2.set_mode("meter_sides")
+
+    meter_fx_r1 = MeterFx(rightMultisegment_1, gradient=g)
     meter_fx_r1.set_mode("meter_sides")
+
+    meter_fx_r2 = MeterFx(rightMultisegment_2, gradient=g)
+    meter_fx_r2.set_mode("meter_sides")
 
     processor = DSPProcessor()
     effectManager = EffectManager(mode="auto_cycle", time_cycle_duration=30000)
     effectManager.add([
         meter_fx_l1,
-        meter_fx_r1
+        meter_fx_l2,
+        meter_fx_r1,
+        meter_fx_r2
     ])
     processor.start()
     loop = Loop(mode="avg", avg_interval=2.0)
@@ -37,7 +68,7 @@ def main():
         while True:
             loop.update()
             processor.update()
-            # loop.print_fps()
+            loop.print_fps()
             effectManager.update(loop.delta, processor.level)
             leftStrip.push()
             rightStrip.push()
